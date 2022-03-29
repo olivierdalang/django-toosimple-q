d23f18cd87e72c7914e35d2ca2d20625142b13a7
olivierdalang <olivier.dalang@gmail.com>
2022-03-29 22:32:39 +0200

    test python code from the readme

---
+++ tests.py
@@ -0,0 +1,25 @@
+import re
+
+from django.test import TestCase
+
+
+class TestReadme(TestCase):
+    """Test code in the readme"""
+
+    def test_readme(self):
+        readme = open("README.md", "r").read()
+
+        # This finds all ```python``` blocks
+        python_blocks = re.findall(r"```python\n([\s\S]*?)```", readme, re.MULTILINE)
+
+        # Run each block
+        full_python_code = ""
+        for python_block in python_blocks:
+
+            # We concatenate all blocks with the previous ones (so we keep imports)
+            full_python_code += python_block
+            try:
+                exec(full_python_code)
+            except Exception as e:
+                hr = "~" * 80
+                raise Exception(f"Invalid readme block:\n{hr}{python_block}{hr}") from e
