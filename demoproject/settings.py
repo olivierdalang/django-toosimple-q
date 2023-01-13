3552f8104c40cecf0b3c6be3a80bd8333ca89b3d
Olivier Dalang <olivier.dalang@gmail.com>
2023-01-13 11:16:47 +0000

    refactor testing framework and remove demoproject

--- demoproject/settings.py
+++
@@ -1,8 +0,0 @@
-from django_toosimple_q.tests.settings import *
-
-# Add the demo and contrib apps
-INSTALLED_APPS = [
-    *INSTALLED_APPS,
-    "demoproject.demoapp",
-    "django_toosimple_q.contrib.mail",
-]
--- django_toosimple_q/tests/demo/tasks.py
+++ django_toosimple_q/tests/demo/tasks.py
@@ -5,17 +5,17 @@ import time
 
 from django.utils import timezone
 
-from django_toosimple_q.decorators import register_task, schedule_task
-from django_toosimple_q.models import TaskExec
+from ...decorators import register_task, schedule_task
+from ...models import TaskExec
 
 
-@schedule_task(cron="* * * * *", datetime_kwarg="scheduled_time")
-@register_task(name="say_hi")
+@schedule_task(cron="*/15 * * * * *", datetime_kwarg="scheduled_time", queue="demo")
+@register_task(name="say_hi", queue="demo")
 def say_hi(scheduled_time):
     return f"Had to say hi {scheduled_time} (it is now {timezone.now()})"
 
 
-@schedule_task(cron="* * * * *", queue="demo")
+@schedule_task(cron="0 * * * * *", queue="demo")
 @register_task(name="flaky", retries=3, retry_delay=2, queue="demo")
 def flaky():
     if random.random() < 0.5:
@@ -24,7 +24,7 @@ def flaky():
         return "This succeeded"
 
 
-@schedule_task(cron="* * * * *", queue="demo")
+@schedule_task(cron="0 * * * * *", queue="demo")
 @register_task(name="logging", queue="demo")
 def logging():
     sys.stdout.write("This should go to standard output")
@@ -32,7 +32,7 @@ def logging():
     return "This is the result"
 
 
-@schedule_task(cron="*/5 * * * *", queue="demo")
+@schedule_task(cron="0 */5 * * * *", queue="demo")
 @register_task(name="long_running", queue="demo")
 def long_running():
     text = f"started at {timezone.now()}\n"
@@ -47,8 +47,8 @@ def long_running():
     return text
 
 
-@schedule_task(cron="*/5 * * * *", run_on_creation=True, queue="demo-cleanup")
-@register_task(name="cleanup", queue="demo-cleanup", priority=-5)
+@schedule_task(cron="0 */30 * * * *", run_on_creation=True, queue="demo")
+@register_task(name="cleanup", queue="demo", priority=-5)
 def cleanup():
     old_tasks_execs = TaskExec.objects.filter(
         created__lte=timezone.now() - datetime.timedelta(minutes=10)
